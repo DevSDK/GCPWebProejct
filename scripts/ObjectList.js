a6ae0304402565eef1b24b720396e369dbc5ad40
1226tmxpvksh <1226tmxpvksh@naver.com>
2019-12-11 15:30:44 +0900

    Object

--- scripts/ObjectList.js
+++ scripts/ObjectList.js
@@ -1,18 +1,22 @@
+//ready 호출//
 $(document).ready(function () {
-
+  //getobjectList 와 같다//
   var objectlist = getObjectList()
 
+  //objectlist길이가 0일때//
   if (objectlist.length == 0) {
 
+    // alert문자열 정의//
     var s = `<div class="alert alert-danger col-lg" role="alert">
       Object Stroage is empty! 
   </div>`
-
+  //뒤에 object-list추가//
     $(".object-list").append(s)
     return
   }
-
+//V에서 objectlist까지 반복//
   for (V of objectlist) {
+    //아래 문자열은 elements에서 설명함//
     var s = `<div class="col-12 col-md-6" style="margin-bottom:30px">
   <div class="card">
            <h5 class="card-header">`+ V.title + '<div class=" float-right badge ' + (V.success ? ' badge-success' : ' badge-danger') + '">' + (V.success ? "SAME" : "DIFF") + `</div> </h5>
@@ -29,7 +33,7 @@ $(document).ready(function () {
               <div class="card border-secondary" style=" margin-bottom: 20px;">
                 <div class="card-header">Com</div>
                 <div class="card-body text-secondary">
-                    <P style=AIzaSyDpFr8CHFycP95gTZ5Dc_G7913fabSYy-QAIzaSyDpFr8CHFycP95gTZ5Dc_G7913fabSYy-Q"height:50px" class="overflow-auto">`+ V.objects.ai + `</P>
+                    <P style= "height:50px" class="overflow-auto">`+ V.objects.ai + `</P>
                 </div>
                 </div>
               </div>  
@@ -51,18 +55,19 @@ $(document).ready(function () {
 
           </div>
       </div>`
-
+      // 뒤에 object-list추가//
     $(".object-list").append(s)
   }
 })
 
-
+// delete_button 누를시 on 호출//
 $(document).on('click', '#delete_button',function () {
+  //are you sure?나오면서 yes or no 출력//
   if (!confirm("are you sure?"))
     return
-
+// 선택한 ObjectList카드 제거//
   removeObjectList($(this).attr("card_id"))
-
+//새로고침//
   location.reload()
 
 })
