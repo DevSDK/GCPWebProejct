9eec0f9609fb50289320aea04c28692305b43ef4
Seokho Song <0xdevssh@gmail.com>
2019-12-12 19:08:28 +0900

    Append all line comment

--- scripts/ObjcetDetecion.js
+++ scripts/ObjcetDetecion.js
@@ -1,133 +1,199 @@
+//Define variables
 var image = null
 var hu_objs = null
 var ai_objs = null
 var ratio = 1
+
+//Convert image to base64 encoded image
 function getBase64(file, callback) {
+    //using renderer class
     var reader = new FileReader();
+    //call Render as URL 
     reader.readAsDataURL(file);
+    //Call back to caller
     reader.onload = function () {
         callback(reader.result);
     };
+    //Error callback
     reader.onerror = function (error) {
-      console.log('Error: ', error);
+        console.log('Error: ', error);
     };
- }
+}
 
-
-$(document).on('input', '#uploadfile', function() {
+//Event handler for fileupload changed
+$(document).on('input', '#uploadfile', function () {
+    //Get text
     var text = $(this).val()
-    if(text.length > 62)
-        text = "..."+text.substring(text.length - 62 , text.length)
+    //Text length limit is 64
+    if (text.length > 62)
+        //Remove long string of filename
+        text = "..." + text.substring(text.length - 62, text.length)
+    //Change text in #file-label
     $("#file-label").text(text)
 })
 
-$(function() {  
+//Event handler for fileupload
+$(function () {
+    //Event handler
     $("#uploadfile").change(function () {
-        getBase64(this.files[0] ,function(ret) {
+        //Change to base64
+        getBase64(this.files[0], function (ret) {
+            //Get canvas context
             var ctx = $("#canv")[0].getContext("2d");
-            image= new Image()
-            image.onload = function() {
-                ctx.canvas.width= 650;
-                ctx.canvas.height= 450;
+            //Create Image class instance
+            image = new Image()
+            //assign callback function on load
+            image.onload = function () {
+                //set size width
+                ctx.canvas.width = 650;
+                //set size height
+                ctx.canvas.height = 450;
+                //calculate ratio
                 var hRatio = ctx.canvas.width / image.width;
+                //calculate raito
                 var vRatio = ctx.canvas.height / image.height;
-                var ratio1  = Math.min ( hRatio, vRatio);
+                //calculate raito
+                var ratio1 = Math.min(hRatio, vRatio);
+                //assign to global variable
                 ratio = ratio1
-              ctx.drawImage(image, 
-                0, 0, image.width * ratio1, image.height*ratio1);
+                //Image draw 
+                ctx.drawImage(image,
+                    0, 0, image.width * ratio1, image.height * ratio1);
             };
-            image.src=ret
+            //Set image
+            image.src = ret
         })
     });
- });
+});
 
-
-$("#image-submit").click(function() {
-    if(image == null)
+//Event handler for #image-submit button
+$("#image-submit").click(function () {
+    //Check nullpointer
+    if (image == null)
         return
-    if($('imp-text') == null)
+    //Check empty text    
+    if ($('imp-text') == null)
         return
-$.ajax (
-    {
-    url:"https://vision.googleapis.com/v1/images:annotate?key=",
-    async:true,
-    type:'POST',
-    dataType:'json',    
-    contentType:"application/json",
-    data: JSON.stringify (   
+    //AJAX HTTP REQUEST
+    $.ajax(
         {
-    
-            requests:[
-            {
-                image:{
-                        content:image.src.split(',')[1]
-                },
+            //set url with api key
+            url: "https://vision.googleapis.com/v1/images:annotate?key=",
+            //Turn on async
+            async: true,
+            //Request type
+            type: 'POST',
+            //Data type
+            dataType: 'json',
+            //Content Type
+            contentType: "application/json",
+            //Set string data using JSON.stringify
+            //Following structure is GCP reqeust speicfication. 
+            data: JSON.stringify(
+                {
+                    requests: [
+                        {
+                            image: {
+                                content: image.src.split(',')[1]
+                            },
 
-                features:[
-                    {
-                        type:"OBJECT_LOCALIZATION"
+                            features: [
+                                {
+                                    type: "OBJECT_LOCALIZATION"
+                                }
+                            ]
+                        }
+
+                    ]
+                }
+            )
+            ,
+            //Success callback
+            success: function (res) {
+                //Clear results
+                $('#aiexp').text('')
+                //Clear results
+                $('#huexp').text('')
+                //get context from canvas
+                var ctx = $("#canv")[0].getContext("2d");
+                //Declare array
+                var ai = []
+                //Foreach responses 
+                for (rs of res.responses) {
+                    //Foreach every objects
+                    for (var obj of rs.localizedObjectAnnotations) {
+                        //Get percentage
+                        var s = obj.name + "(" + Math.round(obj.score * 100) + "%)"
+                        //Set text
+                        $("#aiexp").append(s + ", ")
+                        //push to array
+                        ai.push(s)
+                        //Get vertices
+                        v = obj.boundingPoly.normalizedVertices;
+                        //calculate width
+                        w = Math.abs(v[0].x - v[1].x)
+                        //calculate height
+                        h = Math.abs(v[0].y - v[2].y)
+                        //draw sequance
+                        ctx.beginPath()
+                        //set color
+                        ctx.strokeStyle = "red"
+                        //draw rect
+                        ctx.rect(v[0].x * ratio * image.width, v[0].y * ratio * image.height, w * ratio * image.width, h * ratio * image.height)
+                        //draw
+                        ctx.stroke()
+                        //set fill color
+                        ctx.fillStyle = 'red'
+                        //set font style
+                        ctx.font = "15px Arial"
+                        //draw text
+                        ctx.fillText(obj.name, v[0].x * ratio * image.width, v[0].y * ratio * image.height - 5)
                     }
-                ]
+                }
+                //Copy input 
+                var texts = $("#inp-texts").val().trim().split(',')
+                //Declear array
+                var hu = []
+                //foreach texts
+                for (i in texts) {
+                    //Push to array
+                    hu.push(texts[i])
+                    //check last
+                    if (i == texts.length - 1)
+                        //append without ,
+                        $('#huexp').append(texts[i])
+                    else
+                        //append with ,
+                        $('#huexp').append(texts[i] + ", ")
+                }
+                //refer to hu
+                hu_objs = hu
+                //refer to ai
+                ai_objs = ai
             }
+        })
+})
 
-        ]
-    }
-    )
-    ,
-success:function(res) {
-    $('#aiexp').text('')
-    $('#huexp').text('')
-    var ctx = $("#canv")[0].getContext("2d");
-
-    var ai = []
-    for(rs of res.responses) {
-        for(var obj of rs.localizedObjectAnnotations) {
-            var s = obj.name + "(" +Math.round(obj.score*100)+ "%)"
-            $("#aiexp").append(s  + ", ")
-            ai.push(s)
-                v = obj.boundingPoly.normalizedVertices;
-                w = Math.abs(v[0].x - v[1].x)
-                h = Math.abs(v[0].y - v[2].y)
-
-                ctx.beginPath()
-                ctx.strokeStyle="red"
-                ctx.rect(v[0].x*ratio * image.width, v[0].y*ratio*image.height, w*ratio*image.width,h*ratio*image.height)
-                ctx.stroke()        
-                ctx.fillStyle = 'red'
-                ctx.font = "15px Arial"
-                ctx.fillText(obj.name, v[0].x*ratio * image.width , v[0].y*ratio*image.height - 5)
-        }
-    }
-
-    var texts=  $("#inp-texts").val().trim().split(',')
-    var hu = []
-    for(i in texts)
-    {
-        hu.push(texts[i])
-        if(i == texts.length-1)
-            $('#huexp').append(texts[i])
-        else
-            $('#huexp').append(texts[i]+ ", ")   
-    }
-    hu_objs = hu
-    ai_objs = ai    
-    
-}
-
-})})
-
-
-$('#savebutton').click(function() {
-    var correction = false  
-    if($('input:radio[name="radio"]:checked').attr('id') == "radio-true")
+//Event handler for #savebutton
+$('#savebutton').click(function () {
+    //Declare variable for correction check
+    var correction = false
+    //checking radio button clicked
+    if ($('input:radio[name="radio"]:checked').attr('id') == "radio-true")
+        //set true
         correction = true
     else
+        //set false
         correction = false
-
+    //canvas to ImageURL
     var ctx = $("#canv")[0].toDataURL()
+    //Get title information
     var title = $("#title-inp").val()
-    addObjectList(title,ctx, ai_objs,hu_objs, correction)
+    //Call addObjectList in libs/storage.js
+    addObjectList(title, ctx, ai_objs, hu_objs, correction)
+    //show message "saved"
     alert("saved")
+    //refresh page
     location.reload()
 
 })
